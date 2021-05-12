b926da22a7391c91585506b74e508223f07138c0
yiki <673894879@qq.com>
2021-05-12 08:43:41 +0800

    feat: 增加writeStream和readStream配合使用

--- yiki/文件操作/fswrite.js
+++ yiki/文件操作/fswrite.js
@@ -6,12 +6,14 @@ const ws = fs.createWriteStream(path.resolve(__dirname, './write.txt'), {
   highWaterMark: 2, // 默认16*64 表示预计用多少内存来进行写，但即使超过了还是会往里面写
 })
 
-const rs = fs.createReadStream(path.resolve(__dirname, './write.txt'), {
-  highWaterMark: 3,
+const rs = fs.createReadStream(path.resolve(__dirname, './test.txt'), {
+  highWaterMark: 3, // 当读取大于写入的时候，会进入等待状态
   flags: 'r',
 })
 // write只接受字符串或者buffer
-// flag根据highWaterMark来决定返回的是true或者false，假如超出或等于设定值就会返回false，然后将内容放入栈中，等待执行完再逐步写入，清空缓存区，如果缓存区过大会浪费内存，所以设置highwatermark来进行控制，达到预期后就不要再调用write方法了
+// flag根据highWaterMark来决定返回的是true或者false，
+// 假如超出或等于设定值就会返回false，
+// 然后将内容放入栈中，等待执行完再逐步写入，清空缓存区，如果缓存区过大会浪费内存，所以设置highwatermark来进行控制，达到预期后就不要再调用write方法了
 // 并发异步 =》 串行异步
 // let flag = ws.write('1')
 // console.log(flag)
@@ -28,5 +30,15 @@ const rs = fs.createReadStream(path.resolve(__dirname, './write.txt'), {
 // })
 
 // ws.end() // end + close
+rs.on('data', (chunk) => {
+  console.log(chunk)
+  const flag = ws.write(chunk)
+  if (!flag) {
+    rs.pause()
+  }
+})
 
-rs.on('data', (chunk) => {})
+// 当写入完毕之后，触发的回调
+ws.on('drain', () => {
+  rs.resume()
+})
