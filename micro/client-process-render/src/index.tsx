10848afe71e82defc0c115db88d021c48dd7fe85
xiaoYuanDun <649173212@qq.com>
2021-07-01 18:23:25 +0800

    feat:浏览器渲染部分

---
+++ micro/client-process-render/src/index.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react';
+import ReactDOM from 'react-dom';
+import App1 from './App1';
+import App2 from './App2';
+import App3 from './App3';
+
+// const ele = <App1 />;
+// const ele = <App2 />;
+const ele = <App3 />;
+
+ReactDOM.render(ele, document.getElementById('root'));
---
+++ micro/client-process-render/src/utils.ts
@@ -0,0 +1,8 @@
+const calc = () => {
+  let res;
+  for (let i = 0; i < 599999; i++) {
+    res = `${res}${i}`;
+  }
+};
+
+export { calc };
