30d145d7b100a217086e5851b3256aac4f04acba
xiaoYuanDun <649173212@qq.com>
2021-08-18 18:00:24 +0800

    feat:添加debug测试用例

--- react/debug-react/src/commit/case_2.tsx
+++ react/debug-react/src/commit/case_2.tsx
@@ -1,16 +1,49 @@
-import React, { useEffect, useState, useLayoutEffect } from 'react';
+import React, { useEffect, useState, useLayoutEffect, Component } from 'react';
 
 // useEffect
 
-function Show(props: any) {
+function ShowNum(props: any) {
   return <p>{props.num}</p>;
+}
+
+class SubShowName extends Component<any> {
+  componentWillUnmount() {
+    console.log('class: ----====---->>');
+    console.log('SubShowName componentWillUnmount...');
+  }
+  render() {
+    return <p>{this.props.name}</p>;
+  }
+}
+class ShowName extends Component<any> {
+  getSnapshotBeforeUpdate() {
+    console.log('class: ----====---->>');
+    console.log('getSnapshotBeforeUpdate...');
+    return null;
+  }
+  componentDidUpdate() {
+    console.log('class: ----====---->>');
+    console.log('ShowName componentDidUpdate...');
+  }
+  componentWillUnmount() {
+    console.log('class: ----====---->>');
+    console.log('ShowName componentWillUnmount...');
+  }
+  render() {
+    return (
+      <div>
+        <span>asdasdasd</span>
+        <SubShowName name={this.props.name} />
+      </div>
+    );
+  }
 }
 
 function App() {
   const [num, setNum] = useState(1);
 
   useEffect(() => {
-    console.log('----====---->>');
+    console.log('FC: ----====---->>');
     console.log('every time');
     return () => {
       console.log('every unmount');
@@ -20,7 +53,7 @@ function App() {
   const [name, setName] = useState('xiaoMing');
 
   useEffect(() => {
-    console.log('----====---->>');
+    console.log('FC: ----====---->>');
     console.log('invoke once');
     return () => {
       console.log('---------------------------------------------FC unmount');
@@ -28,7 +61,7 @@ function App() {
   }, []);
 
   useLayoutEffect(() => {
-    console.log('----====---->>');
+    console.log('FC: ----====---->>');
     console.log('every useLayoutEffect');
     return () => {
       console.log('every layout unmount');
@@ -37,11 +70,14 @@ function App() {
 
   const handleClick = () => {
     setNum(num + 1);
+    setName(name === 'danny' ? 'danny' : 'xiaoMing');
   };
+
   return (
     <div>
       <button onClick={handleClick}>click me</button>
-      {num % 3 === 0 ? null : <Show num={num} />}
+      {num % 3 === 0 ? null : <ShowNum num={num} />}
+      {num % 2 === 0 ? null : <ShowName name={name} />}
     </div>
   );
 }
